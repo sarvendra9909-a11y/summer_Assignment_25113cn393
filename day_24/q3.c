5c948a576166bbcfe169425dc800d2a000478be2
sarvendra9909-a11y <sarvendra9909@gmail.com>
2026-06-22 14:37:59 +0530

    Added day_24 C Programs

---
+++ day_24/q3.c
@@ -0,0 +1,29 @@
+// WAP TO FIND A LONGEST WORD.
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100], longest[100];
+    int i, len = 0, max = 0, start = 0;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        if(str[i] != ' ' && str[i] != '\n') {
+            len++;
+        } else {
+            if(len > max) {
+                max = len;
+                strncpy(longest, &str[start], len);
+                longest[len] = '\0';
+            }
+            len = 0;
+            start = i + 1;
+        }
+    }
+
+    printf("Longest word = %s", longest);
+
+    return 0;
+}
